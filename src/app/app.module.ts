bd94b9fb68048b140e36129dae59f20e2a91107c
JamieB <jamie.bowen@forgerock.com>
2018-10-17 23:17:15 +0100

    Added bootstrap styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { DegMinInputComponent } from './deg-min-input/deg-min-input.component';
@@ -12,7 +13,8 @@ import { DegMinInputComponent } from './deg-min-input/deg-min-input.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
